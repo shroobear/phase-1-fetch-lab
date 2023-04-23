276fa1148beb6765e6e70e49efd92de6cc3dec16
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-04-23 19:15:06 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function(books) {
+      renderBooks(books)
+    })
+  };
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -9,9 +15,9 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
